2ac9474eb77ddc1ef6284e10ea748d5d995c4206
usama1998-pro <proattackers69@gmail.com>
2023-12-23 22:46:10 +0500

    vectore 1 imroved, prompt tweaked!

--- vectors.py
+++ vectors.py
@@ -8,25 +8,16 @@ import io
 load_dotenv()
 
 
-string = """*Model Name: Three Universal Outcomes
-Components: Always Ready, Always Responsive, and Always Innovative outcomes
-Use: The Three Universal Outcomes guide structuring interactions that enable sustainability for the customers, organization, and workforce regardless of the future.
-Additional Information:
-URL_FOR_THIS_MODEL: https://enterpriseagility.community/three-universal-outcomes-of-enterprise-agility-10fx9k4s11
-Autor: Erich R. Bühler and Enterprise Agility University
-*End of Model Description"""
-
-
 # print(len(string))
 
-corpus_file = "corpus.txt"
-vec_name = "AgilityModelVectorStore"
+corpus_file = "corpus_info.txt"
+vec_name = "AgilityModelInfoVectorStore"
 
 print(f"Reading File [{corpus_file}]...")
 corpus = io.open(corpus_file, "r", encoding="utf-8").read()
 
 
-size = 700
+size = 2000
 print(f"Splitting Text [chunk-size={size}]...")
 # Splitting document in to Text
 text_splitter = CharacterTextSplitter(chunk_overlap=0, chunk_size=size, length_function=len)
